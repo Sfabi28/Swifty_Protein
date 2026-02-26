7cfbcfbc8e300a82ccc5a305633a7aa64d4c742a
fgori42 <98gori@gmail.com>
2026-02-26 15:48:30 +0100

    aggiunto database

---
+++ app/src/main/java/com/example/swifty_protein/DbHelper.kt
@@ -0,0 +1,46 @@
+package com.example.swifty_protein
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.content.ContentValues
+import android.database.sqlite.SQLiteOpenHelper
+
+
+class DbHelper (private val context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+    companion object{
+        const val DATABASE_NAME = "mydatabase.db"
+        const val DATABASE_VERSION = 1
+
+        const val TABLE_USERS = "users"
+        const val COLUMN_ID = "_id"
+        const val COLUMN_NAME = "name"
+        const val COLUMN_PASSWORD = "password"
+    }
+
+    override fun onCreate(db: SQLiteDatabase){
+        val CREATE_TABLE_USERS = """CREATE TABLE $TABLE_USERS (
+           $COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+           $COLUMN_NAME TEXT UNIQUE,
+           $COLUMN_PASSWORD TEXT)"""
+        db.execSQL(CREATE_TABLE_USERS)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int){
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_USERS")
+        onCreate(db)
+    }
+
+    fun addUser(name: String, password: String): Long {
+        if (name.isBlank() || password.isBlank()) {
+            return -1L
+        }
+        val db = this.writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_NAME, name)
+            put(COLUMN_PASSWORD, password)
+        }
+        val id = db.insert(TABLE_USERS, null, values)
+        db.close()
+        return id
+    }
+}
--- app/src/main/java/com/example/swifty_protein/MainActivity.kt
+++ app/src/main/java/com/example/swifty_protein/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,16 +13,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.swifty_protein.ui.theme.Swifty_ProteinTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import kotlin.math.max
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val text = String
         setContent {
             Swifty_ProteinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +45,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    val text = String
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Swifty_Protein",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Swifty_ProteinTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
