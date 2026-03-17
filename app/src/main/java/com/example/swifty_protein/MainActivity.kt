6b89ea892032b64964ac9014b1a52c6abe62fcf5
fgori <98gori@gmail.com>
2026-03-17 16:15:48 +0100

    primo push di m6 filtro funziona e messo su home

--- app/src/main/java/com/example/swifty_protein/MainActivity.kt
+++ app/src/main/java/com/example/swifty_protein/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,9 +21,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.LaunchedEffect
@@ -40,6 +44,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.input.pointer.pointerInput
 
 class LigandViewModel : ViewModel() {
     // Questo è lo stato che la UI osserverà
@@ -89,17 +103,102 @@ fun LoginScreen(onNavigateToHome: () -> Unit) {
     }
 
 }
+@Composable
+fun SearchBarLigand(onValueChange: (String) -> Unit){
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+    val notFoundText = stringResource(R.string.notFound)
+    val listOfLigands = remember { mutableStateListOf<String>() }
+    LaunchedEffect(Unit) {
+        try {
+            context.assets.open("ligands.txt").bufferedReader().useLines { lines ->
+                lines.forEach { listOfLigands.add(it) }
+            }
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    var isFocused by remember { mutableStateOf(false) }
+    var text by remember { mutableStateOf("") }
+
+    val listToShow = remember(text, listOfLigands.size){
+        if (text.isBlank()) listOfLigands.toList()
+        else {
+            val filtered = listOfLigands.filter { it.contains(text.trim(), ignoreCase = true) }
+            filtered.ifEmpty {
+                listOf(notFoundText)
+            }
+
+        }
+    }
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Top
+    ){
+        TextField(
+            value = text,
+            onValueChange = {text = it},
+            label = { Text("Filtra Ligandi") },
+            placeholder = { Text("Es: 1EJ") },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                }
+
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        if (isFocused){
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = 200.dp)
+            ) {
+                items(listToShow) { ligand ->
+                    val isNotFound = ligand == notFoundText
+                    Text(
+                        text = ligand,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(!isNotFound) {
+                                onValueChange(ligand)
+                                text = ligand
+                                focusManager.clearFocus()
+                            }
+                            .padding(16.dp),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = if (isNotFound) Color.Gray else Color.Unspecified
+                    )
+                    HorizontalDivider(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        thickness = 0.5.dp
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Composable //proto home screen
 fun HomeScreen(onBack: () -> Unit) {
     val viewModel = remember { LigandViewModel() }
     var searchId by remember { mutableStateOf("") }
     val state = viewModel.ligandData
+    val focusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
+            .padding(24.dp)
+            .pointerInput(Unit) {
+                detectTapGestures ( onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -108,17 +207,10 @@ fun HomeScreen(onBack: () -> Unit) {
         Spacer(modifier = Modifier.height(20.dp))
 
         // Input per l'ID del ligando (es. 1EJ, ABC, 001)
-        TextField(
-            value = searchId,
-            onValueChange = { searchId = it },
-            label = { Text("ID Ligando") },
-            placeholder = { Text("Es: 1EJ") },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-
         Spacer(modifier = Modifier.height(10.dp))
 
+
+        SearchBarLigand(onValueChange = { searchId = it })
         Button(
             onClick = {
                 if (searchId.isNotBlank()) {
@@ -129,6 +221,8 @@ fun HomeScreen(onBack: () -> Unit) {
         ) {
             Text(text = "Scarica Dati .CIF")
         }
+
+
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -177,6 +271,6 @@ fun HomeScreen(onBack: () -> Unit) {
 @Composable // la preview e statica percui possiamo decidere che screen visualizzare ma per un vero test c'è da emulare
 fun LoginScreenPreview() {
     Swifty_ProteinTheme {
-        LoginScreen(onNavigateToHome = {})
+        HomeScreen(onBack = {})
     }
 }
