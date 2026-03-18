37e3a7081e03b49ca1fb1d5045fb4f82f47e70d2
Sfabi28 <samuelefabi28@gmail.com>
2026-03-18 15:12:06 +0100

    Revert "bug fixes on auto logout"
    This reverts commit af434c13ffdebc4dbaea8c77d7b136ced4c4cde1.

--- app/src/main/java/com/example/swifty_protein/data/SessionManager.kt
+++ app/src/main/java/com/example/swifty_protein/data/SessionManager.kt
@@ -1,24 +1,21 @@
 package com.example.swifty_protein.data
 
 import android.content.Context
-import android.content.SharedPreferences
 
 class SessionManager(context: Context) {
-    private val sharedPreferences: SharedPreferences =
-        context.getSharedPreferences("SwiftyProteinPrefs", Context.MODE_PRIVATE)
+    private val prefs = context.getSharedPreferences("swifty_prefs", Context.MODE_PRIVATE)
 
     fun setLogin(isLoggedIn: Boolean) {
-        sharedPreferences.edit().putBoolean("KEY_IS_LOGGED_IN", isLoggedIn).commit()
+        prefs.edit().putBoolean("is_logged_in", isLoggedIn).apply()
     }
 
-    fun isLoggedIn(): Boolean {
-        return sharedPreferences.getBoolean("KEY_IS_LOGGED_IN", false)
-    }
+    fun isLoggedIn(): Boolean = prefs.getBoolean("is_logged_in", false)
+
     fun saveLastUser(username: String) {
-        sharedPreferences.edit().putString("last_logged_user", username).apply()
+        prefs.edit().putString("last_logged_user", username).apply()
     }
 
     fun getLastUser(): String? {
-        return sharedPreferences.getString("last_logged_user", null)
+        return prefs.getString("last_logged_user", null)
     }
 }
